5ea1436ecf07d44c8caa92b76baabcaff587c54d
ManavParmar010 <manavparmar2003@gmail.com>
2024-04-03 13:23:54 +0530

    create post album and getalbumbyid function

--- main.go
+++ main.go
@@ -19,7 +19,10 @@ var albums = []album{
 }
 func main(){
 	router := gin.Default()
+	
 	router.GET("/albums",getAlbums)
+	router.POST("/albums",postAlbum)
+	router.GET("/albums/:id",getAlbumByID)
 
 	router.Run("localhost:8001")
 }
@@ -28,3 +31,24 @@ func getAlbums (c *gin.Context){
 	c.IndentedJSON(http.StatusOK,albums)
 }
 
+func postAlbum(c *gin.Context){
+	var newAlbum album
+	if err:= c.BindJSON(&newAlbum); err != nil{
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated,newAlbum);
+}
+
+func getAlbumByID(c *gin.Context){
+	id := c.Param("id")
+
+	for _,a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK,a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound,gin.H{"message":"album not found"})
+}
