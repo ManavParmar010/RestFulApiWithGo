dad2350741508fc9a51e8fa29c5b2403344db9a3
ManavParmar010 <manavparmar2003@gmail.com>
2024-04-03 12:20:18 +0530

    modify main.go

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ type album struct{
 	ID string `json:"id"`
 	Title string `json:"title"`
 	Artist string `json:"Artist"`
-	Price string `json:"Price"`
+	Price float64 `json:"Price"`
 }
 
 var albums = []album{
-	{ID: "1",Title: "Blue Train",}
+	{ID: "1",Title: "Blue Train",Artist: "John Coltrane",Price:56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
